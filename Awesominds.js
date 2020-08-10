8b996f26de28fae305389dbed759e65f87247ce0
Adam Lowe <adamlowe2259@gmail.com>
2020-08-10 10:58:28 -0700

    made the links open up in a new tab will add the scrum meeting notes later

--- Awesominds.js
+++ Awesominds.js
@@ -14,40 +14,40 @@ function validation() {
 		document.getElementById("WSR").innerHTML = "<h1>Weekly Status Report</h1>";
 		
 		//Week 1 WSR
-		document.getElementById("WSR").innerHTML += "<div><a href = 'includes/WSR/WSR1.pdf'> Weekly Status Report #1</div>";
+		document.getElementById("WSR").innerHTML += "<div><a href = 'includes/WSR/WSR1.pdf' target = '_blank'> Weekly Status Report #1</div>";
 		document.getElementById("WSR").innerHTML += "<hr>";
 		//Week 2 WSR
-		document.getElementById("WSR").innerHTML += "<div><a href = 'includes/WSR/WSR2.pdf'> Weekly Status Report #2</div>";
+		document.getElementById("WSR").innerHTML += "<div><a href = 'includes/WSR/WSR2.pdf' target = '_blank'> Weekly Status Report #2</div>";
 		document.getElementById("WSR").innerHTML += "<hr>";
 		//Week 3 WSR
-		document.getElementById("WSR").innerHTML += "<div><a href = 'includes/WSR/WSR3.pdf'> Weekly Status Report #3</div>";
+		document.getElementById("WSR").innerHTML += "<div><a href = 'includes/WSR/WSR3.pdf' target = '_blank'> Weekly Status Report #3</div>";
 		document.getElementById("WSR").innerHTML += "<hr>";
 		//Week 4 WSR
-		document.getElementById("WSR").innerHTML += "<div><a href = 'includes/WSR/WSR4.pdf'> Weekly Status Report #4</div>";
+		document.getElementById("WSR").innerHTML += "<div><a href = 'includes/WSR/WSR4.pdf' target = '_blank'> Weekly Status Report #4</div>";
 		document.getElementById("WSR").innerHTML += "<hr>";
 		//Week 5 WSR
-		document.getElementById("WSR").innerHTML += "<div><a href = 'includes/WSR/WSR5.pdf'> Weekly Status Report #5</div>";
+		document.getElementById("WSR").innerHTML += "<div><a href = 'includes/WSR/WSR5.pdf' target = '_blank'> Weekly Status Report #5</div>";
 		document.getElementById("WSR").innerHTML += "<hr>";
 		//Week 6 WSR
-		document.getElementById("WSR").innerHTML += "<div><a href = 'includes/WSR/WSR6.pdf'> Weekly Status Report #6</div>";
+		document.getElementById("WSR").innerHTML += "<div><a href = 'includes/WSR/WSR6.pdf' target = '_blank'> Weekly Status Report #6</div>";
 		document.getElementById("WSR").innerHTML += "<hr>";
 		//Week 7 WSR
-		document.getElementById("WSR").innerHTML += "<div><a href = 'includes/WSR/WSR7.pdf'> Weekly Status Report #7</div>";
+		document.getElementById("WSR").innerHTML += "<div><a href = 'includes/WSR/WSR7.pdf' target = '_blank'> Weekly Status Report #7</div>";
 		document.getElementById("WSR").innerHTML += "<hr>";
 		//Week 8 WSR
-		document.getElementById("WSR").innerHTML += "<div><a href = 'includes/WSR/WSR8.pdf'> Weekly Status Report #8</div>";
+		document.getElementById("WSR").innerHTML += "<div><a href = 'includes/WSR/WSR8.pdf' target = '_blank'> Weekly Status Report #8</div>";
 		document.getElementById("WSR").innerHTML += "<hr>";
 		//Week 9 WSR
-		document.getElementById("WSR").innerHTML += "<div><a href = 'includes/WSR/WSR9.pdf'> Weekly Status Report #9</div>";
+		document.getElementById("WSR").innerHTML += "<div><a href = 'includes/WSR/WSR9.pdf' target = '_blank'> Weekly Status Report #9</div>";
 		document.getElementById("WSR").innerHTML += "<hr>";
 		//Week 10 WSR
-		document.getElementById("WSR").innerHTML += "<div><a href = 'includes/WSR/WSR10.pdf'> Weekly Status Report #10</div>";
+		document.getElementById("WSR").innerHTML += "<div><a href = 'includes/WSR/WSR10.pdf' target = '_blank'> Weekly Status Report #10</div>";
 		document.getElementById("WSR").innerHTML += "<hr>";
 		//Week 11 WSR
-		document.getElementById("WSR").innerHTML += "<div><a href = 'includes/WSR/WSR11.pdf'> Weekly Status Report #11</div>";
+		document.getElementById("WSR").innerHTML += "<div><a href = 'includes/WSR/WSR11.pdf' target = '_blank'> Weekly Status Report #11</div>";
 		document.getElementById("WSR").innerHTML += "<hr>";
 		//Week 12 WSR
-		document.getElementById("WSR").innerHTML += "<div><a href = 'includes/WSR/WSR12.pdf'> Weekly Status Report #12</div>";
+		document.getElementById("WSR").innerHTML += "<div><a href = 'includes/WSR/WSR12.pdf' target = '_blank'> Weekly Status Report #12</div>";
 		document.getElementById("WSR").innerHTML += "<hr>";
 		//bottom of WSR
 		// document.getElementById("WSR").innerHTML += "<hr style='height:2px;border-width:0;color:gray;background-color:gray'>";
@@ -115,49 +115,49 @@ function validation() {
 		//Minutes Sprint 1
 		document.getElementById("Minutes").innerHTML += "<h3>Sprint 1</h3>";
 		//Minutes 1
-		document.getElementById("Minutes").innerHTML += "<div><a href = 'includes/Minutes/Meeting1.pdf'> Meeting Minutes 1</div>";
+		document.getElementById("Minutes").innerHTML += "<div><a href = 'includes/Minutes/Meeting1.pdf' target = '_blank'> Meeting Minutes 1</div>";
 		//Minutes 2
-		document.getElementById("Minutes").innerHTML += "<div><a href = 'includes/Minutes/Meeting2.pdf'> Meeting Minutes 2</div>";
+		document.getElementById("Minutes").innerHTML += "<div><a href = 'includes/Minutes/Meeting2.pdf' target = '_blank'> Meeting Minutes 2</div>";
 		document.getElementById("Minutes").innerHTML += "<hr>";
 		
 		//Minutes Sprint 2
 		document.getElementById("Minutes").innerHTML += "<h3>Sprint 2</h3>";
 		//Minutes 3
-		document.getElementById("Minutes").innerHTML += "<div><a href = 'includes/Minutes/Meeting3.pdf'> Meeting Minutes 3</div>";
+		document.getElementById("Minutes").innerHTML += "<div><a href = 'includes/Minutes/Meeting3.pdf' target = '_blank'> Meeting Minutes 3</div>";
 		//Minutes 4
-		document.getElementById("Minutes").innerHTML += "<div><a href = 'includes/Minutes/Meeting4.pdf'> Meeting Minutes 4</div>";
+		document.getElementById("Minutes").innerHTML += "<div><a href = 'includes/Minutes/Meeting4.pdf' target = '_blank'> Meeting Minutes 4</div>";
 		document.getElementById("Minutes").innerHTML += "<hr>";
 		
 		//Minutes Sprint 3
 		document.getElementById("Minutes").innerHTML += "<h3>Sprint 3</h3>";
 		//Minutes 5
-		document.getElementById("Minutes").innerHTML += "<div><a href = 'includes/Minutes/Meeting5.pdf'> Meeting Minutes 5</div>";
+		document.getElementById("Minutes").innerHTML += "<div><a href = 'includes/Minutes/Meeting5.pdf' target = '_blank'> Meeting Minutes 5</div>";
 		//Minutes 6
-		document.getElementById("Minutes").innerHTML += "<div><a href = 'includes/Minutes/Meeting6.pdf'> Meeting Minutes 6</div>";
+		document.getElementById("Minutes").innerHTML += "<div><a href = 'includes/Minutes/Meeting6.pdf' target = '_blank'> Meeting Minutes 6</div>";
 		document.getElementById("Minutes").innerHTML += "<hr>";
 		
 		//Minutes Sprint 4
 		document.getElementById("Minutes").innerHTML += "<h3>Sprint 4</h3>";
 		//Minutes 7
-		document.getElementById("Minutes").innerHTML += "<div><a href = 'includes/Minutes/Meeting7.pdf'> Meeting Minutes 7</div>";
+		document.getElementById("Minutes").innerHTML += "<div><a href = 'includes/Minutes/Meeting7.pdf' target = '_blank'> Meeting Minutes 7</div>";
 		//Minutes 8
-		document.getElementById("Minutes").innerHTML += "<div><a href = 'includes/Minutes/Meeting8.pdf'> Meeting Minutes 8</div>";
+		document.getElementById("Minutes").innerHTML += "<div><a href = 'includes/Minutes/Meeting8.pdf' target = '_blank'> Meeting Minutes 8</div>";
 		document.getElementById("Minutes").innerHTML += "<hr>";
 		
 		//Minutes Sprint 5
 		document.getElementById("Minutes").innerHTML += "<h3>Sprint 5</h3>";
 		//Minutes 9
-		document.getElementById("Minutes").innerHTML += "<div><a href = 'includes/Minutes/Meeting9.pdf'> Meeting Minutes 9</div>";
+		document.getElementById("Minutes").innerHTML += "<div><a href = 'includes/Minutes/Meeting9.pdf' target = '_blank'> Meeting Minutes 9</div>";
 		//Minutes 10
-		document.getElementById("Minutes").innerHTML += "<div><a href = 'includes/Minutes/Meeting10.pdf'> Meeting Minutes 10</div>";
+		document.getElementById("Minutes").innerHTML += "<div><a href = 'includes/Minutes/Meeting10.pdf' target = '_blank'> Meeting Minutes 10</div>";
 		document.getElementById("Minutes").innerHTML += "<hr>";
 		
 		//Minutes Sprint 6
 		document.getElementById("Minutes").innerHTML += "<h3>Sprint 6</h3>";
 		//Minutes 11
-		document.getElementById("Minutes").innerHTML += "<div><a href = 'includes/Minutes/Meeting11.pdf'> Meeting Minutes 11</div>";
+		document.getElementById("Minutes").innerHTML += "<div><a href = 'includes/Minutes/Meeting11.pdf' target = '_blank'> Meeting Minutes 11</div>";
 		//Minutes 12
-		document.getElementById("Minutes").innerHTML += "<div><a href = 'includes/Minutes/Meeting12.pdf'> Meeting Minutes 12</div>";
+		document.getElementById("Minutes").innerHTML += "<div><a href = 'includes/Minutes/Meeting12.pdf' target = '_blank'> Meeting Minutes 12</div>";
 		
 		//Bottom of Minutes Section
 		document.getElementById("Minutes").innerHTML += "<hr style='height:2px;border-width:0;color:gray;background-color:gray'>";
